bc407a3e4fe56fcd4dfaaac72b6c4423900589d7
shurik204 <iwmdiif@gmail.com>
2024-12-26 23:03:15 +0200

    Added Maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "me.shurik"
@@ -16,4 +17,28 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    if (System.getenv("MAVEN_URL") != null) {
+        publications {
+            create<MavenPublication>("jar") {
+                repositories {
+                    maven(System.getenv("MAVEN_URL")) {         // Maven repository URL
+                        credentials {
+                            username=System.getenv("MAVEN_USER")
+                            password=System.getenv("MAVEN_PASSWORD")
+                        }
+                    }
+                }
+                groupId = group.toString()
+                artifactId = project.name
+
+                // Includes jar, sources and dependencies
+                from(project.components["java"])
+            }
+        }
+    } else {
+        logger.warn("[!] Maven URL is not set, skipped setting up Maven publishing")
+    }
 }
